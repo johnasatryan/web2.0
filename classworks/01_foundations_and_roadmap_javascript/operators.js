2ec5c99b1f21e94fb5cb4fbd942a585caee5ec74
johnasatryan <john.asatryan@gmail.com>
2025-06-03 11:16:12 +0400

    02.06

---
+++ classworks/01_foundations_and_roadmap_javascript/operators.js
@@ -0,0 +1,124 @@
+// // let b = 3;
+// // console.log(~b);
+
+// // let x = -6;
+// // console.log(-x);
+
+// // let x = {};
+// // console.log(!x);
+
+// // function fib() {
+// //   // if (1 > 0) {
+// //   //   let x = 23;
+// //   //   return;
+// //   // }
+// //   void x;
+// //   return x;
+// // // }
+
+// // // // console.log(void (2 === '2'));
+
+// // // // void function foo() {};
+
+// // // // console.log(foo());
+
+// // // console.log(typeof '6.5');
+// // // console.log(typeof []);
+// // // console.log(typeof fib);
+// // // console.log(typeof {});
+// // // console.log(typeof undefined);
+// // // console.log(typeof null);
+
+// // // // struct null { };
+// // // // struct undefined {};
+
+// // // // let x = null;
+// // // // if (x) {
+
+// // // // }
+
+// // // let person = {
+// // //   name: 'James',
+// // //   age: 21,
+// // // };
+
+// // // console.log(person);
+
+// // // delete person.age;
+// // // console.log(person);
+
+// // // delete person;
+// // // console.log(person);
+
+// // // let arr = [1, 3];
+
+// // // arr[9] = 'hello';
+
+// // // console.log(arr);
+
+// // // delete person.name;
+
+// // // console.log(5 ** 2);
+// // // console.log(Math.pow(5, 2));
+
+// // // console.log(6 + 5 * 2)/
+
+// // // let x = 6;
+// // // x %= 2;
+// // // console.log(Math.round(3.5));
+// // // console.log(Math.floor(3.9));
+// // // console.log(Math.ceil(3.9));
+
+// // // console.log(5 + 1 + "23" + 6 - 1)
+
+// // // let x = Number.MAX_VALUE;
+// // // console.log(x);
+
+// // // 5..toString()
+
+// // // let x = -1;
+// // // console.log(x >>> 0);
+
+// // let arr = [];
+
+// // function foo() {}
+// // console.log(arr instanceof Object);
+// // console.log(1 instanceof Object);
+// // console.log('1' instanceof Object);
+// // console.log(Date instanceof Object);
+// // console.log(null instanceof Object);
+// // console.log(foo instanceof Object);
+// // console.log(new Number() instanceof Object);
+// // const obj = { age: 23 };
+// // const a = Symbol('hello');
+
+// // const person = { age: 23 };
+// // console.log('age' in person);
+// // console.log('hobby' in person);
+// // console.log(person.hobby);
+
+// // person.hobby = 'dev';
+
+// // console.log(undefined == null);
+// // console.log(undefined === null);
+// // console.log(2 == '2');
+// // console.log(2 === '2');
+
+// let a = 4; // Binary: 0100
+// let b = 2; // Binary: 0010
+// let c = 1; // Binary: 0001
+
+// let result = a || b & c;
+// console.log(result);
+
+// let nullish = null;
+// let undefinish = null;
+
+// let x = 1;
+// let y = 0;
+
+// // console.log(nullish ?? undefinish);
+
+// x *= 2 + 1;
+
+
