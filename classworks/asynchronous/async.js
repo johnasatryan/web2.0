e130d704be74375a8f122db0f9600f83a7fcd33d
johnasatryan <john.asatryan@gmail.com>
2025-08-16 14:44:00 +0400

    practice

--- classworks/asynchronous/async.js
+++ classworks/asynchronous/async.js
@@ -1,66 +1,152 @@
-// // Transformation of async/await (Promise)
+// // // Transformation of async/await (Promise)
 
-// async function f() {
-//   console.log('Hello world');
-//   return 'Line 5';
-// }
-
-// // function f() {
+// // async function f() {
 // //   console.log('Hello world');
-
-// //   return Promise.resolve('Line 11');
+// //   return 'Line 5';
 // // }
 
-// console.log('Line 7');
-// const res = f();
-// res.then((value) => {
-//   console.log(value);
-// });
-// console.log('Line 9');
+// // // function f() {
+// // //   console.log('Hello world');
 
-// custom_fetch('https');
+// // //   return Promise.resolve('Line 11');
+// // // }
 
-// function custom_fetch(url) {
-//   const promise = new Promise(function executor(resolve, reject) {
-//     if (url.startsWith('https')) {
-//       resolve({ status: 'Ok', user: { name: 'James', age: 12 } });
-//     } else {
-//       reject({
-//         status: 'Failed',
-//         message: 'You are not allowed to use another protocol',
-//       });
-//     }
-//   });
+// // console.log('Line 7');
+// // const res = f();
+// // res.then((value) => {
+// //   console.log(value);
+// // });
+// // console.log('Line 9');
 
-//   return promise;
+// // custom_fetch('https');
+
+// // function custom_fetch(url) {
+// //   const promise = new Promise(function executor(resolve, reject) {
+// //     if (url.startsWith('https')) {
+// //       resolve({ status: 'Ok', user: { name: 'James', age: 12 } });
+// //     } else {
+// //       reject({
+// //         status: 'Failed',
+// //         message: 'You are not allowed to use another protocol',
+// //       });
+// //     }
+// //   });
+
+// //   return promise;
+// // }
+
+// // function f(url) {
+// //   const result = custom_fetch(url);
+// //   result
+// //     .then((value) => {
+// //       console.log(value);
+// //     })
+// //     .catch((err) => {
+// //       console.log(err.message);
+// //     });
+// // }
+
+// // f('ftp://jsonplaceholder.typicode/posts');
+
+// // async function custom_fetch(url) {
+// //   try {
+// //     var x = 23;
+// //     const fetched_result = await fetch(url);
+// //     console.log(fetched_result);
+// //     return;
+// //   } catch (err) {
+// //     console.log(err.message);
+// //     console.log(x);
+// //     return;
+// //   } finally {
+// //     console.log('hello finally');
+// //   }
+// // }
+
+// // custom_fetch('ftp://jsonplaceholder.typicode.com/posts');
+
+// // const promise = new Promise((resolve, reject) => {
+// //   setTimeout(() => console.log('Executor'), 0);
+
+// //   resolve('Resolve');
+// // });
+
+// // console.log('Before promise');
+
+// // promise.then((val) => {
+// //   console.log(val);
+// // });
+// // console.log('After promise');
+
+// async function foo() {
+//   console.log('1');
+//   await console.log('5');
+//   Promise.resolve();
+//   console.log('2');
+//   return 'Promise object';
 // }
 
-// function f(url) {
-//   const result = custom_fetch(url);
-//   result
-//     .then((value) => {
-//       console.log(value);
-//     })
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
+// // function foo() {
+// //   console.log('1');
+// //   Promise.resolve().then(() => {
+// //     console.log('2');
+// //   });
+
+// //   return Promise.resolve();
+// // }
+
+// // console.log('3');
+// // foo().then((value) => {
+// //   console.log(value);
+// // });
+// // console.log('4');
+
+// // async function resolve_data() {
+// //   const result = await fetch('https://google.com');
+// //   return result;
+// // }
+
+// // resolve_data().then((value) => {
+// //   console.log(value);
+// // });
+
+// function getUser() {
+//   let promises = [];
+
+//   for (let i = 0; i < 3; ++i) {
+//     const promise = fetch(
+//       `https://jsonplaceholder.typicode.com/users/${i + 1}`,
+//     );
+//     promises.push(promise);
+//   }
+//   return promises;
+// }
+// async function getResults() {
+//   const result = await getUser();
+
 // }
 
-// f('ftp://jsonplaceholder.typicode/posts');
+// getResults();
 
-async function custom_fetch(url) {
-  try {
-    var x = 23;
-    const fetched_result = await fetch(url);
-    console.log(fetched_result);
-    return;
-  } catch (err) {
-    console.log(err.message);
-    console.log(x);
-    return;
-  } finally {
-    console.log('hello finally');
-  }
-}
+// const fs = require('node:fs');
 
-custom_fetch('ftp://jsonplaceholder.typicode.com/posts');
+// let file_data = '';
+// const data = fs.readFileSync('index.html');
+
+const promise = new Promise((resolve, reject) => {
+  resolve(1);
+});
+
+promise
+  .then((value) => {
+    console.log(value);
+    return 2;
+  })
+  .then((value) => {
+    console.log(value);
+
+    return 3;
+  })
+  .then((value) => {
+    console.log(value);
+  });
--- classworks/asynchronous/promise.js
+++ classworks/asynchronous/promise.js
@@ -1,42 +1,48 @@
-class MyPromiseComplete {
+class MyPromise {
   constructor(executor) {
-    this.state = 'pending';
-    this.value = undefined;
-    this.reason = undefined;
-    this.onFulfilledCallbacks = [];
-    this.onRejectedCallbacks = [];
-    this.hasUnhandledRejection = false;
+    this.state = 'pending'; // current default state
+    this.value = undefined; // catch resolve value
+    this.reason = undefined; // catch reject value
+
+    this.onfulfilledCallbacks = [];
+    this.onrejectedCallbacks = [];
+    this.rejectedFlag = false;
 
     const resolve = (value) => {
       if (this.state !== 'pending') return;
       this.state = 'fulfilled';
       this.value = value;
 
-      this.onFulfilledCallbacks.forEach((callback) => {
-        queueMicrotask(() => callback(this.value));
+      queueMicrotask(() => {
+        this.onfulfilledCallbacks.forEach((cb) => {
+          cb(this.value);
+        });
       });
     };
 
     const reject = (reason) => {
       if (this.state !== 'pending') return;
+
+      console.log(this.onrejectedCallbacks);
       this.state = 'rejected';
       this.reason = reason;
 
-      if (this.onRejectedCallbacks.length > 0) {
-        this.onRejectedCallbacks.forEach((callback) => {
-          queueMicrotask(() => callback(this.reason));
+      if (this.onrejectedCallbacks.length > 0) {
+        this.onrejectedCallbacks.forEach((cb) => {
+          queueMicrotask(() => {
+            cb(this.reason);
+          });
         });
       } else {
-        // Schedule unhandled rejection detection
-        this.hasUnhandledRejection = true;
+        this.rejectedFlag = true;
         queueMicrotask(() => {
-          if (this.hasUnhandledRejection) {
-            console.error('Unhandled Promise Rejection:', this.reason);
+          console.log(this.rejectedFlag);
+          if (this.rejectedFlag) {
+            console.error('UnhandledPromiseRejection', this.reason);
           }
         });
       }
     };
-
     try {
       executor(resolve, reject);
     } catch (err) {
@@ -45,109 +51,74 @@ class MyPromiseComplete {
   }
 
   then(onFulfilled, onRejected) {
-    // Always return a NEW promise for chaining
-    return new MyPromiseComplete((resolve, reject) => {
-      const handleFulfilled = (value) => {
+    return new MyPromise((resolve, reject) => {
+      const handlerFulfilled = (value) => {
         try {
-          if (typeof onFulfilled === 'function') {
-            const result = onFulfilled(value);
-            resolve(result);
-          } else {
-            resolve(value); // Pass through if no handler
+          if (typeof onFulfilled !== 'function') {
+            resolve(value); // mi kentronaceq
           }
-        } catch (error) {
-          reject(error);
+          queueMicrotask(() => {
+            const result = onFulfilled(this.value);
+            resolve(result);
+          });
+        } catch (err) {
+          onRejected(err);
         }
       };
-
-      const handleRejected = (reason) => {
-        this.hasUnhandledRejection = false; // We have a handler now
+      const handlerRejected = (reason) => {
+        this.rejectedFlag = false;
         try {
-          if (typeof onRejected === 'function') {
-            const result = onRejected(reason);
-            resolve(result); // Note: resolve, not reject!
-          } else {
-            reject(reason); // Pass through if no handler
+          if (typeof onRejected !== 'function') {
+            reject(reason); // mi kentronaceq
           }
-        } catch (error) {
-          reject(error);
+          queueMicrotask(() => {
+            const result = onRejected(this.reason);
+            resolve(result);
+          });
+        } catch (err) {
+          reject(err);
         }
       };
 
       if (this.state === 'fulfilled') {
-        queueMicrotask(() => handleFulfilled(this.value));
+        queueMicrotask(() => {
+          handlerFulfilled(this.value);
+        });
       } else if (this.state === 'rejected') {
-        queueMicrotask(() => handleRejected(this.reason));
+        queueMicrotask(() => {
+          handlerRejected(this.reason);
+        });
       } else {
-        // Still pending
-        this.onFulfilledCallbacks.push(handleFulfilled);
-        this.onRejectedCallbacks.push(handleRejected);
+        this.onfulfilledCallbacks.push(onFulfilled);
+        this.onrejectedCallbacks.push(onRejected);
       }
     });
   }
-
   catch(onRejected) {
     return this.then(null, onRejected);
   }
-
-  finally(onFinally) {
-    return this.then(
-      (value) => {
-        if (typeof onFinally === 'function') onFinally();
-        return value;
-      },
-      (reason) => {
-        if (typeof onFinally === 'function') onFinally();
-        throw reason;
-      },
-    );
-  }
 }
 
-console.log('\n=== Testing Complete Version ===');
+// const p = new MyPromise((resolve) => {
+//   resolve('some_value');
+// });
 
-// Test chaining
-new MyPromiseComplete((resolve, reject) => {
-  resolve(10);
-})
-  .then((value) => {
-    console.log('Chain step 1:', value);
-    return value * 2;
-  })
-  .then((value) => {
-    console.log('Chain step 2:', value);
-    return value + 5;
-  })
-  .then((value) => {
-    console.log('Chain step 3:', value);
-  });
+// const r = p
+//   .then((value) => {
+//     console.log(value);
+//     return 23;
+//   })
+//   .then((value) => {
+//     console.log(value);
+//   })
+//   .then();
 
-// Test error handling in chain
-new MyPromiseComplete((resolve, reject) => {
-  reject('Initial error');
-})
-  .catch((error) => {
-    console.log('Caught in chain:', error);
-    return 'recovered';
-  })
-  .then((value) => {
-    console.log('After recovery:', value);
-  });
+// console.log(r);
 
-setTimeout(() => {
-  console.log('\n=== Creating unhandled rejection ===');
-  new MyPromiseComplete((resolve, reject) => {
-    reject('This will be unhandled');
-  });
-}, 100);
-
-console.log('\n=== Your Original Code Fixed ===');
-
-const p1 = new MyPromiseComplete(function executor(resolve, reject) {
-  if (5 < 0) {
-    resolve('Everything is ok');
-  } else {
-    reject('type must be string');
-  }
+const p2 = new MyPromise((resolve, reject) => {
+  reject('hello');
 });
 
+p2.catch((value) => {
+  console.log(value);
+});
