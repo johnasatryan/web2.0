4c3ea37c45d75074f5123e0f8783989e21b17b28
johnasatryan <john.asatryan@gmail.com>
2025-08-23 12:09:43 +0400

    22.08

---
+++ classworks/html/script.js
@@ -0,0 +1,21 @@
+// // WeakMap, WeakSet
+
+// // Map, Set
+
+// const users = [];
+
+// let userData = { name: 'James', age: 23 };
+// users.push(userData);
+
+// // userData = null;
+
+// console.log(users);
+
+// const cachedUsers = [];
+
+// let weakUser = new WeakMap();
+
+// weakUser.set(userData, 'value');
+// userData = null;
+
+// console.log(weakUser.get(userData));
